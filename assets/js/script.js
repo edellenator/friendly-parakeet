c426f99c4796e9f8deee28a8d6e34ffd024a457d
Erik Dell <edellenator@gmail.com>
2022-03-24 19:49:40 -0700

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+// Assignment code here
+
+
+// Get references to the #generate element
+// var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+
+// }
+
+// Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
+
+
+function makeid() {
+  var text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (var i = 0; i < 5; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+  return text;
+}
+
+console.log(makeid());
+// " !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
